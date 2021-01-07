b5f9f51d8067cbf96baa6122eaf0b18cb1f43ad7
shabnam <shabnam.mostary@gmail.com>
2021-01-06 20:58:57 -0500

    updated server.js,models,ontrollers files for newer ES features

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const { getAllAuthors, getAuthorByIdOrName } = require('./controllers/authors')
-const { getAllGenres, getGenreById } = require('./controllers/genres')
-const { getAllNovels, getNovelByIdOrTitle } = require('./controllers/novels')
+import express from 'express'
+import path from 'path'
+import { getAllAuthors, getAuthorByIdOrName } from './controllers/authors'
+import { getAllGenres, getGenreById } from './controllers/genres'
+import { getAllNovels, getNovelByIdOrTitle } from './controllers/novels'
 
 const app = express()
 
@@ -14,6 +15,7 @@ app.get('/api/genres/:id', getGenreById)
 
 app.get('/api/novels', getAllNovels)
 app.get('/api/novels/:identifier', getNovelByIdOrTitle)
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
